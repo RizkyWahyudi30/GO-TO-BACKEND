3ebf6b41cf1e15a587c501b7c8c6289bcf63cb3d
rizkywahyudi <rizkywahyudi302008@gmail.com>
2025-07-31 17:22:27 +0700

    step by step

--- day-2/project-improve/script.js
+++ day-2/project-improve/script.js
@@ -7,4 +7,9 @@ function pesanTiket() {
     alert("Nama atau umur tidak boleh kosong!");
     return;
   }
+
+  const now = new Date();
+  const bookingTime = new Date(now.getTime() + 1 * 60 * 1000);
+
+  document.getElementById("");
 }
