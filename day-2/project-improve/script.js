0edf4b851e639944fac3bb342ed1ba5a5eeae013
rizkywahyudi <rizkywahyudi302008@gmail.com>
2025-08-01 17:24:56 +0700

    step by step

--- day-2/project-improve/script.js
+++ day-2/project-improve/script.js
@@ -11,5 +11,31 @@ function pesanTiket() {
   const now = new Date();
   const bookingTime = new Date(now.getTime() + 1 * 60 * 1000);
 
-  document.getElementById("");
+  document.getElementById("jadwalBooking").innerHTML = `
+  <h3>Tiket Anda:</h3>
+  <p>Nama: ${namaUser}</p>
+  <p>Umur: ${umurUser}</p>
+  <p>Jadwal Booking: ${bookingTime.toLocaleTimeString()}</p>
+  `;
+
+  const countdownTime = document.getElementById("countdown");
+  const interval = setInterval(() => {
+    const noew = new Date();
+    const selisihWaktu = bookingTime - new Date();
+
+    if (selisihWaktu <= 0) {
+      clearInterval(interval);
+      countdownTime.textContent = "Waktu booking telah berakhir.";
+    } else {
+      const detik = Math.floor((selisihWaktu / 1000) % 60);
+      const menit = Math.floor((selisihWaktu / 1000 / 60) % 60);
+      const jam = Math.floor((selisihWaktu / 1000 / 60 / 60) % 60);
+
+      countdownTime.textContent = `Waktu tersisa ${jam
+        .toString()
+        .padStart(2, "0")}:${menit.toString().padStart(2, "0")}:${detik
+        .toString()
+        .padStart(2, "0")}`;
+    }
+  }, 1000);
 }
---
+++ netcad.js
@@ -0,0 +1,18 @@
+// question 1
+const rosePrice = 8;
+const lilyPrice = 10;
+const tulipPrice = 2;
+
+let quatintyRose = 70;
+let quatintyLily = 50;
+let quatintyTulip = 120;
+// value
+let valueRose = rosePrice * quatintyRose;
+console.log(valueRose);
+let valueLily = lilyPrice * quatintyLily;
+console.log(valueLily);
+let valueTulip = tulipPrice * quatintyTulip;
+console.log(valueTulip);
+// total
+let totalValue = valueRose + valueLily + valueTulip;
+console.log(totalValue);
